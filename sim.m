6176b0caedfbe977029d0e9e4855c73409a4a473
Lalith Suresh <suresh.lalith@gmail.com>
2014-11-27 13:40:55 +0100

    n x m clients

--- sim.m
+++ sim.m
@@ -1,76 +1,81 @@
 clear;
 pkg load odepkg;
 
-T = 1:100;
+T = 1:600;
 
-function qdot = q(t, x, xd)
-	lamb = lambda(t);
+function qdot = q(t, x, xd,  num_clients, num_servers, lags, qsz_exponent)
 
-	measured_q1 = max(xd(1, 1), 0);
-	measured_q2 = max(xd(2, 2), 0);
+	arrival_rate = ArrivalRate(t, num_clients);
+	flow_matrix = zeros(num_clients, num_servers);
+	for client = 1:num_clients
+		qmu_inverse = zeros(num_servers, 1);
+		for server = 1:num_servers
+			measured_q = max(xd(server, server), 0);
+			% 1/rate == mu
+			% need a way to import time lag
+			qmu_inverse(server) = ServiceRate(t - lags(server), num_servers)(server)/((1 + measured_q) ^ qsz_exponent);
+		endfor
+		TotalWeight = sum(qmu_inverse);
 
-	% 1/rate == mu
-	qmu_1 = ((1 + measured_q1)^3)/service_rate(t - 1)(1);
-	qmu_2 = ((1 + measured_q2)^3)/service_rate(t - 1)(2);
-	C = qmu_2/qmu_1;
-
-	f1 = C * lamb / (1 + C);
-	f2 = lamb - f1;
+		% xxx: use vector operation
+		for server = 1:num_servers
+			flow_fraction = qmu_inverse(server)/(TotalWeight);
+			flow_matrix(client, server) = arrival_rate(client) * flow_fraction;
+		endfor
+	endfor
+	flow_matrix;
 
 	% use the current rate to find the integral
-	rate = service_rate(t);
-	rate_1 = rate(1);
-	if (x(1) <= 0)
-		rate_1 = 0;
-	endif
-
-	rate_2 = rate(2);
-	if (x(2) <= 0)
-		rate_2 = 0;
-	endif
+	rate = ServiceRate(t, num_servers);
 
 	% printf("%d %d %d %d %d %d %d %d\n", x(1), x(2), t, f1, f2, xd(2, 2), xd(1, 1), lamb);
-	qdot(1) = f1 - rate_1;
-	qdot(2) = f2 - rate_2;
+
+	for i = 1:num_servers
+		if (x(i) <= 0)
+			rate(i) = 0;
+		endif
+		qdot(i) = sum(flow_matrix(:, i)) - rate(i);
+	endfor
 endfunction
 
-function lambda = lambda(t)
-	lamb = 50;
-
-	% if (t > 30)
-	% 	lamb = 50;
-	% elseif (t > 20)
-	% 	lamb = 20;
-	% endif
-
-	lambda(1) = lamb;
+function ArrivalRate = ArrivalRate(t, num_clients)
+	lamb = 30;
+	ArrivalRate = repmat(lamb, 1, num_clients);
+	% ArrivalRate = [20; 20; 20];
 endfunction
 
-function service_rate = service_rate(t)
+% xxx: use sinusoid
+function ServiceRate = ServiceRate(t, num_servers)
+	
+	ServiceRate = repmat(50, 1, num_servers);
 
-	st_1 = 35;
-	st_2 = 15;
-
-	if (t > 30)
-		st_1 = 15;
-		st_2 = 35;
-	elseif (t > 20)
-		st_1 = 35;
-		st_2 = 25;
+	if(t > 400)
+		ServiceRate = [90;50;10];
+	elseif(t > 100)
+		ServiceRate = [30;70;50];
 	endif
 
-	service_rate(1) = st_1;
-	service_rate(2) = st_2;
-
+	% st = 50;
+	% amplitude = 5;
+	% ServiceRate = [sin(t/10 + pi/4) * amplitude + st; 
+	% 			   sin(t/10 + pi/2) * amplitude + st; 
+	% 			   sin(t/10 + 3* pi/4) * amplitude + st];
 endfunction
 
 initq = 500;
-x0_1 = initq;
-x0_2 = initq;
+num_clients = 5;
+num_servers = 3;
+qsz_exponent = 3;
 
-hist_mat = repmat(initq, 2, 3);
+% xxx: add estimation error to qsz
 
-res = ode23d(@q, T, [x0_1; x0_2], [1, 1], hist_mat);
+init = repmat([initq], num_servers, 1);
+% lags = repmat([1], 1, num_servers);
+lags = [0.5, 0.5, 0.5];
+
+hist_mat = repmat(initq, num_servers, num_servers);
+
+res = ode23d(@q, T, init, lags, hist_mat, num_clients, num_servers, lags, qsz_exponent);
 
 % XXX: Add legend
-plot(res.x, res.y)
+plot(res.x, res.y, 'LineWidth', 2);
