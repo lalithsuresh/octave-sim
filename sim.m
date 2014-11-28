1af1b407642ce218b1769af66530c8142d22c912
Lalith Suresh <suresh.lalith@gmail.com>
2014-11-28 10:29:36 +0100

    Temp commit

--- sim.m
+++ sim.m
@@ -7,9 +7,10 @@ NumT = Tend * 2;
 T = linspace(Tstart, Tend, NumT);
 
 function qdot = q(t, x, xd,  num_clients, num_servers, lags, qsz_exponent, os)
-
+	qdot = zeros(num_servers + num_clients, 1);
 	arrival_rate = ArrivalRate(t, num_clients);
 	flow_matrix = zeros(num_clients, num_servers);
+	sending_rate = [10; 10; 10]
 	for client = 1:num_clients
 		qmu_inverse = zeros(num_servers, 1);
 		for server = 1:num_servers
@@ -23,30 +24,76 @@ function qdot = q(t, x, xd,  num_clients, num_servers, lags, qsz_exponent, os)
 		TotalWeight = sum(qmu_inverse);
 
 		% xxx: use vector operation
-		for server = 1:num_servers
-			flow_fraction = qmu_inverse(server)/(TotalWeight);
-			flow_matrix(client, server) = arrival_rate(client) * flow_fraction;
+		% backlogSize = x(num_servers + client);
+		backlogSize = 0;
+		total_to_allocate = 0;
+		if (x(num_servers + client) > 0)
+			% then sum of sending rates
+			if (t > 402)
+				sending_rate(1) = 56/5;
+			endif
+			total_to_allocate = sum(sending_rate);
+		else
+			total_to_allocate = arrival_rate(client);
+		endif
+		is_full = zeros(num_servers, 1);
+	
+		for k = 1:num_servers	
+			for server = 1:num_servers
+				if(is_full(server) == 0)
+					flow_fraction = qmu_inverse(server)/(TotalWeight);
+					flow_matrix(client, server) = (backlogSize + total_to_allocate) * flow_fraction;
+					% backlogSize + total_to_allocate, flow_fraction
+					if (flow_matrix(client, server) > sending_rate(server))
+						backlogSize = backlogSize + (flow_matrix(client, server) - sending_rate(server));
+						flow_matrix(client, server) = sending_rate(server);
+						is_full(server) = 1;
+						TotalWeight = TotalWeight - qmu_inverse(server);
+						total_to_allocate = total_to_allocate - sending_rate(server);
+					endif
+				endif
+			endfor
+
+			if (backlogSize == 0)
+				break
+			endif
 		endfor
+
+		if (t > 401)
+			flow_matrix
+		endif
+		% Update backlog size at client
+		total_drain = sum(flow_matrix(client, :));
+		% if (x(num_servers + client) <= 0)
+		% 	total_drain = 0
+		% endif
+		qdot(num_servers + client) = arrival_rate(client) - total_drain;
 	endfor
-	flow_matrix;
+	flow_matrix
 
 	% use the current rate to find the integral
 	rate = ServiceRate(t, num_servers);
 
 	% printf("%d %d %d %d %d %d %d %d\n", x(1), x(2), t, f1, f2, xd(2, 2), xd(1, 1), lamb);
 
-	for i = 1:num_servers
-		if (x(i) <= 0)
-			rate(i) = 0;
+	for server = 1:num_servers
+		if (x(server) <= 0)
+			rate(server) = 0;
 		endif
-		qdot(i) = sum(flow_matrix(:, i)) - rate(i);
+		qdot(server) = sum(flow_matrix(:, server)) - rate(server);
 	endfor
 endfunction
 
 function ArrivalRate = ArrivalRate(t, num_clients)
 	lamb = 30;
 	ArrivalRate = repmat(lamb, 1, num_clients);
-	% ArrivalRate = [20; 20; 20];
+
+	if(t > 401)
+		ArrivalRate = repmat(lamb, 1, num_clients);
+	elseif(t > 400)
+		ArrivalRate = repmat(31, 1, num_clients);
+	endif
+
 endfunction
 
 % xxx: use sinusoid
@@ -54,11 +101,12 @@ function ServiceRate = ServiceRate(t, num_servers)
 	
 	ServiceRate = repmat(50, 1, num_servers);
 
-	if(t > 400)
-		ServiceRate = [90;50;10];
+	if(t > 401)
+		ServiceRate = [60;50;50];
 		% ServiceRate = [30;70;50];
 	elseif(t > 100)
-		ServiceRate = [30;70;50];
+		% ServiceRate = [30;70;50];
+		ServiceRate = [50;50;50];
 	endif
 
 	% st = 50;
@@ -68,18 +116,24 @@ function ServiceRate = ServiceRate(t, num_servers)
 	% 			   sin(t/10 + 3* pi/4) * amplitude + st];
 endfunction
 
-initq = 500;
+initServerQ = 500;
+initClientQ = 0;
 num_clients = 5;
 num_servers = 3;
 qsz_exponent = 1;
 os = 0;
 
-init = repmat([initq], num_servers, 1);
+init = vertcat(repmat([initServerQ], num_servers, 1), 
+			   repmat([initClientQ], num_clients, 1));
 lags = repmat([0.5], 1, num_servers);
 
-hist_mat = repmat(initq, num_servers, num_servers);
+hist_mat = vertcat(repmat(initServerQ, num_servers, num_servers),
+				   repmat(initClientQ, num_clients, num_servers));
 
 res = ode23d(@q, T, init, lags, hist_mat, num_clients, num_servers, lags, qsz_exponent, os);
 
 % XXX: Add legend
-plot(res.x, res.y, 'LineWidth', 2);
+subplot (2, 1, 1);
+plot(res.x, res.y(:, 1:num_servers), 'LineWidth', 2);
+subplot (2, 1, 2);
+plot(res.x,  res.y(:, num_servers + 1 :num_servers + num_clients), 'LineWidth', 2);
